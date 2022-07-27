57ac503f5b653dd52a55bfed6a3c30d3b23a140f
Beau Bouchard <bnb2011@gmail.com>
2022-07-27 14:00:38 -0400

    feat: added some slices

--- src/middleware/slackResponse.ts
+++ src/middleware/slackResponse.ts
@@ -1,1 +1,14 @@
 // This will convert a normal get response to just have a few things which are requred to pass thru slack
+/**
+ *
+ * @param {Request} request - Express request object
+ * @param {Response} response - Express response object
+ * @param {NextFunction} next - Express next function
+ */
+ function slackResponse(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+
+}
